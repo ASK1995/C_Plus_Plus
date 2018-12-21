f4ed907d82a0b419a9a7370a4fae31dd77785eea
Santosh Kumar <a.santoshkumar1995@gmail.com>
2018-12-20 20:02:40 -0800

    Create initials.cpp

---
+++ initials.cpp
@@ -0,0 +1,17 @@
+//I am going to be the very best
+
+#include <iostream>
+
+int main() {
+
+  // Your program lives here:
+	std::cout<<"ASK\n";
+  std::cout<<"  A    SSS  K   K\n";
+  std::cout<<" A A  S   S K  K\n";
+  std::cout<<"A   A S     K K\n";
+  std::cout<<"AAAAA  SSS  KK\n";
+  std::cout<<"A   A     S K K\n";
+  std::cout<<"A   A S   S K  K\n";
+  std::cout<<"A   A  SSS  K   K\n";
+  
+}
